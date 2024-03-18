8820141a2f7c0c088e164db71079ec48c5810487
Muralikrishna91 <163506610+Muralikrishna91@users.noreply.github.com>
2024-03-18 17:36:42 +0530

    Update B.java

--- B.java
+++ B.java
@@ -3,5 +3,6 @@ public class B
    public static void main(String[] args)
      {
          System.out.println("hello Murali Krishna ");
+        System.out.println("This is edit after enter the Repository");
       }
   }
